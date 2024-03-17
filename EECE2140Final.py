3a0c7157bc2cb02368f1a999a2ad657e6b45f079
wcxman <christman.w@northeastern.edu>
2024-03-17 16:50:09 -0400

    Working CSV file importation

--- EECE2140Final.py
+++ EECE2140Final.py
@@ -8,9 +8,33 @@ class dataset:
     def __init__(self, filename, frequency):
         with open(str(filename), mode = 'r') as file: #Opens the CSV file in a read mode
             self.csvfile = csv.reader(file) #Sets this object's data set to the contents of the CSV file
+            self.data = [[],[],[]] #Creates a variable for data, holding time, input voltage, and output voltage
+            count = 0
+            for line in self.csvfile: #For each line in the file
+                count += 1
+                if count >= 12: #If the line is 12 or higher
+                    self.data[0].append(float(line[0])) #Adds the time to the time slot of data
+                    self.data[1].append(float(line[1])) #Adds the in voltage to that slot
+                    self.data[2].append(float(line[2])) #Adds out voltage etc.
+            self.ts = self.data[0] #Makes own list for each variable for ease of use
+            self.vins = self.data[1]
+            self.vouts = self.data[2]
         self.frq = frequency
     def curvefit():
         return 0 #to implement
+    def adddata(self,filename): #Method to add more data to the object's data with another CSV
+        with open(str(filename),mode = 'r') as file: #Opens the CSV file in a read mode
+            thisfile = csv.reader(file) #Creates a new file object for this file
+            count = 0
+            for line in thisfile:
+                count += 1
+                if count >= 12: #If the line is 12 or higher
+                    self.data[0].append(float(line[0])) #Adds the time to the time slot of data
+                    self.data[1].append(float(line[1])) #Adds the in voltage to that slot
+                    self.data[2].append(float(line[2])) #Adds out voltage etc.
+            self.ts = self.data[0] #Makes own list for each variable for ease of use
+            self.vins = self.data[1]
+            self.vouts = self.data[2]
     def stdv(thelist):
         sum = 0 #Sum of the list
         for i in thelist:
@@ -20,4 +44,3 @@ class dataset:
         for i in thelist: #For each value in the list
             numerator += (i - avg)**2 #Adds the difference between the value at the array and the average, all squared
         return math.sqrt(numerator/(len(thelist)-1)) #Returns the formula for standard deviation
-        
