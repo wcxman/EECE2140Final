3d6e7c6740418c6d1594d430784a36b7b255a413
wcxman <christman.w@northeastern.edu>
2024-03-25 18:22:25 -0400

    Added Gaussian fitting, a method for histogram fitting, the code is fully functional to plot gaussian distributions of voltages in, out, and drops using either histograms or full scatterplots of data.

--- EECE2140Final.py
+++ EECE2140Final.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import scipy
+from scipy.optimize import curve_fit 
 import math
 
 class dataset:
@@ -21,8 +21,6 @@ class dataset:
             self.vouts = self.data[2]
         self.frq = frequency
         self.name = name
-    def curvefit():
-        return 0 #to implement
     def adddata(self,filename): #Method to add more data to the object's data with another CSV
         with open(str(filename),mode = 'r') as file: #Opens the CSV file in a read mode
             thisfile = csv.reader(file) #Creates a new file object for this file
@@ -51,14 +49,36 @@ class dataset:
             tor.append(self.vouts[i]-self.vins[i]) #Adds the voltage drop of this datapoint
         return tor #Returns the list
     def makehistogram(self):
-        plt.hist(self.vdrops(),bins=100) #Creates a histogram of the voltage drops in the dataset
+        plt.hist(self.vouts,bins=np.linspace(-2,2,100)) #Creates a histogram of the voltage drops in the dataset
         plt.xlabel("Change in voltage (V)") #Labeling and titling the graph in following lines
         plt.ylabel("Frequency (Number)")
         plt.title("Frequency of voltage drops in " + self.name)
         plt.show()
-        
+    def histogramdata(self,startbound,endbound,detail): #Very inefficient way to sort data into bins, to be improved in a later version
+        xdata = np.linspace(startbound,endbound,detail) #Every bin for the X value
+        raw = [] #A storage of the quantities of values in each bin
+        totalsum = 0 #The total sum of all data points looked at
+        for i in range(0,len(xdata)): #For each x data
+            count = 0 #Number of points that fit
+            for j in self.vouts: #For each voltage out
+                if i is not len(xdata)-1:
+                    if j >= xdata[i] and j < xdata[i+1]: #If this value is within this bin
+                        count+=1 #Adds one to both the total count and this bin's count
+                        totalsum+=1
+                else:
+                    if j >= xdata[i] and j < xdata[i]+((endbound-startbound)/detail):
+                        count+=1
+                        totalsum+=1
+            raw.append(count) #Sets this bin's total amount of points to the raw data list
+        tor = [] #To return
+        for i in raw:
+            tor.append(i/totalsum) #Percent of points that appear in this bin
+        return tor
+def Gauss(x, A, B): #Function for the form of Gaussian distribution, x is x values and A and B are arrays returned by the curve fit function
+    y = A*np.exp((-1*B*x**2)) #
+    return y
 def main():
-    data = dataset("acq0001.csv",10,"Plot Name")
+    data = dataset("acq0001.csv",10,"10Hz Trial 2") #Imports every file from acq0001 to acq0100, can be modified for any range of files
     for i in range(2,10):
         file = "acq000" + str(i) + ".csv"
         data.adddata(file)
@@ -67,5 +87,11 @@ def main():
         data.adddata(file)
     data.adddata("acq0100.csv")
     data.makehistogram()
-    
+    xdata = data.ts #The x data of the plot
+    ydata = data.vins #The y data of the scatterplot
+    popt, pcov = curve_fit(Gauss, xdata, ydata, p0=[1,1]) #Using the curve fit method to get the 
+    xplot = np.linspace(min(xdata), max(xdata), 100) #Makes 100 x values within the bounds of xdata for a plot
+    plt.plot(xdata, ydata) #Makes a scatterplot of x and y values
+    plt.plot(xplot, Gauss(xplot, *popt), 'r-') #Plots the gaussian distribution of the data
+
 main()
